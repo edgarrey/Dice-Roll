d1a1835b228064d55421c03336be2055fb8d9f12
Edgar <some1just@gmail.com>
2020-05-15 14:52:44 -0600

    added addition to an array

--- assets/java.js
+++ assets/java.js
@@ -1,1 +1,27 @@
-.
+const rollButton = document.querySelector('#roll')
+const dieRolls = []
+let textInput = document.querySelector('#inputtext')
+const results = document.querySelector("#rolls")
+let diceRoll = 
+
+
+rollButton.addEventListener("click", function (textInput) {
+   
+   
+   
+    var iRandomNumber;
+   
+   iRandomNumber = Math.ceil(Math.random()*6);
+
+   console.log(iRandomNumber)
+})
+
+
+results.addEventListener("click", function(){
+
+})
+
+
+var sum = dieRolls.reduce(function(a, b) {
+    return a + b;
+}, 0);
